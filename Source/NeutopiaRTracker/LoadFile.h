f731ece7f5b13c80ffe309e7fdbc39bb1ecde49e
Dogveloper <dogveloperlive@gmail.com>
2020-07-23 02:30:46 -0300

    Automation Starts
    Added item automation: now all items will toggle on at item get if the "AutoTrack" is set to on. Map Nodes still as a manual toggle for now.

---
+++ Source/NeutopiaRTracker/LoadFile.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "LoadFile.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEUTOPIARTRACKER_API ULoadFile : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
